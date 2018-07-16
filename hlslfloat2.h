436aa83e54b04865df6a0dc2e450c9db4247bc70
MaiHD <maihd.dev@gmail.com>
2018-07-16 17:20:26 +0700

    Add simple test

--- hlslfloat2.h
+++ hlslfloat2.h
@@ -25,7 +25,37 @@ inline float2 operator*(const float2& a, const float2& b)
 
 inline float2 operator/(const float2& a, const float2& b)
 {
-    return float2(a.x / b.x, a.x / b.y);
+    return float2(a.x / b.x, a.y / b.y);
+}
+
+inline bool2 operator==(const float2& a, const float2& b)
+{
+    return bool2(a.x == b.x, a.y == b.y);
+}
+
+inline bool2 operator!=(const float2& a, const float2& b)
+{
+    return bool2(a.x != b.x, a.y != b.y);
+}
+
+inline bool2 operator<(const float2& a, const float2& b)
+{
+    return bool2(a.x < b.x, a.y < b.y);
+}
+
+inline bool2 operator>(const float2& a, const float2& b)
+{
+    return bool2(a.x > b.x, a.y > b.y);
+}
+
+inline bool2 operator<=(const float2& a, const float2& b)
+{
+    return bool2(a.x <= b.x, a.y <= b.y);
+}
+
+inline bool2 operator>=(const float2& a, const float2& b)
+{
+    return bool2(a.x >= b.x, a.y >= b.y);
 }
 
 inline float2 cos(const float2& v)
