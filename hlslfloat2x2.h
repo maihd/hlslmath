df2a92179a1c215a53ab2ab6285dd7974d3c97b4
MaiHD <maihd.dev@gmail.com>
2018-07-17 17:10:43 +0700

    float2x2 operators

--- hlslfloat2x2.h
+++ hlslfloat2x2.h
@@ -4,4 +4,173 @@
 #include "hlslfloat.h"
 #include "hlslfloat2.h"
 
+inline float2x2 operator-(const float2x2& m)
+{
+    float2x2 result;
+    result[0] = -m[0];
+    result[1] = -m[1];
+    return result;
+}
+
+inline const float2x2& operator+(const float2x2& m)
+{
+    return m;
+}
+
+inline float2x2& operator--(float2x2& m)
+{
+    return m;
+}
+
+inline float2x2& operator++(float2x2& m)
+{
+    return m;
+}
+
+inline float2x2 operator+(const float2x2& a, const float2x2& b)
+{
+    float2x2 result;
+    result[0] = a[0] + b[0];
+    result[1] = a[1] + b[1];
+    return result;
+}
+
+inline float2x2 operator-(const float2x2& a, const float2x2& b)
+{
+    float2x2 result;
+    result[0] = a[0] - b[0];
+    result[1] = a[1] - b[1];
+    return result;
+}
+
+inline float2x2 operator*(const float2x2& a, const float2x2& b)
+{
+    float2x2 result;
+    result[0] = a[0] * b[0];
+    result[1] = a[1] * b[1];
+    return result;
+}
+
+inline float2x2 operator/(const float2x2& a, const float2x2& b)
+{
+    float2x2 result;
+    result[0] = a[0] / b[0];
+    result[1] = a[1] / b[1];
+    return result;
+}
+
+inline float2x2 operator+(const float2x2& a, float b)
+{
+    float2x2 result;
+    result[0] = a[0] + b;
+    result[1] = a[1] + b;
+    return result;
+}
+
+inline float2x2 operator-(const float2x2& a, float b)
+{
+    float2x2 result;
+    result[0] = a[0] - b;
+    result[1] = a[1] - b;
+    return result;
+}
+
+inline float2x2 operator*(const float2x2& a, float b)
+{
+    float2x2 result;
+    result[0] = a[0] * b;
+    result[1] = a[1] * b;
+    return result;
+}
+
+inline float2x2 operator/(const float2x2& a, float b)
+{
+    float2x2 result;
+    result[0] = a[0] / b;
+    result[1] = a[1] / b;
+    return result;
+}
+
+inline float2x2 operator+(float a, const float2x2& b)
+{
+    float2x2 result;
+    result[0] = a + b[0];
+    result[1] = a + b[1];
+    return result;
+}
+
+inline float2x2 operator-(float a, const float2x2& b)
+{
+    float2x2 result;
+    result[0] = a - b[0];
+    result[1] = a - b[1];
+    return result;
+}
+
+inline float2x2 operator*(float a, const float2x2& b)
+{
+    float2x2 result;
+    result[0] = a * b[0];
+    result[1] = a * b[1];
+    return result;
+}
+
+inline float2x2 operator/(float a, const float2x2& b)
+{
+    float2x2 result;
+    result[0] = a / b[0];
+    result[1] = a / b[1];
+    return result;
+}
+
+inline bool2x2 operator==(const float2x2& a, const float2x2& b)
+{
+    bool2x2 result;
+    result[0] = a[0] == b[0];
+    result[1] = a[1] == b[1];
+    return result;
+}
+
+inline bool2x2 operator!=(const float2x2& a, const float2x2& b)
+{
+    bool2x2 result;
+    result[0] = a[0] != b[0];
+    result[1] = a[1] != b[1];
+    return result;
+}
+
+inline bool2x2 operator<(const float2x2& a, const float2x2& b)
+{
+    bool2x2 result;
+    result[0] = a[0] < b[0];
+    result[1] = a[1] < b[1];
+    return result;
+}
+
+inline bool2x2 operator>(const float2x2& a, const float2x2& b)
+{
+    bool2x2 result;
+    result[0] = a[0] > b[0];
+    result[1] = a[1] > b[1];
+    return result;
+}
+
+inline bool2x2 operator<=(const float2x2& a, const float2x2& b)
+{
+    bool2x2 result;
+    result[0] = a[0] <= b[0];
+    result[1] = a[1] <= b[1];
+    result[2] = a[2] <= b[2];
+    result[3] = a[3] <= b[3];
+    return result;
+}
+
+inline bool2x2 operator>=(const float2x2& a, const float2x2& b)
+{
+    bool2x2 result;
+    result[0] = a[0] >= b[0];
+    result[1] = a[1] >= b[1];
+    return result;
+}
+
 #endif /* __HLSL_FLOAT2X2_H__ */
