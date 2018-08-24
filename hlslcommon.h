ef3cf0377e73173eda0f52ca30ff37b85e74c83a
MaiHD <maihd.dev@gmail.com>
2018-08-24 10:07:51 +0700

    Change PI template to const

--- hlslcommon.h
+++ hlslcommon.h
@@ -5,38 +5,38 @@
 #undef min // When Windows.h is included, min is an macro
 #undef max // When Windows.h is included, max is an macro
 
-template <typename T>
-inline T PI()
-{
-    return 3.14159265358979f;
-}
+/* PI number (float)
+ */
+static const float PI = 3.14159265358979f;
 
-template <>
-inline double PI()
-{
-    return 3.14159265358979;
-}
-
+/* Convert radians to degrees (float)
+ */
 inline float degrees(float x)
 {
-    const float factor = 180.0f / PI<float>();
+    const float factor = 180.0f / PI;
     return x * factor;
 }
 
+/* Convert degrees to radians (float)
+ */
 inline float radians(float x)
 {
-    const float factor = PI<float>() / 180.0f;
+    const float factor = PI / 180.0f;
     return x * factor;
 }
 
+/* Convert radians to degrees (double)
+ */
 inline double degrees(double x)
 {
-    const double factor = 180.0 / PI<double>();
+    const double factor = 180.0 / PI;
     return x * factor;
 }
 
+/* Convert degrees to radians (double)
+ */
 inline double radians(double x)
 {
-    const double factor = PI<double>() / 180.0f;
+    const double factor = PI / 180.0;
     return x * factor;
 }
