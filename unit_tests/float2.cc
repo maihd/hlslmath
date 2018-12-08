7ce43c8b9be698f117e8fb2fc2c04f5779634aee
MaiHD <maihd.dev@gmail.com>
2018-12-08 14:33:30 +0700

    Add more unit tests to float2

--- unit_tests/float2.cc
+++ unit_tests/float2.cc
@@ -3,10 +3,13 @@
 
 int main(int argc, char* argv[])
 {
-    float2 a = float2(0);
-    float2 b = float2(0);
+    float2 a = float2(0, 0);
+    float2 b = float2(0, 0);
 
     test(all(a == b), "a should equal to b");
+    test(!any(a != b), "a should equal to b");
+
+    test(all(a + b == float2(0, 0)), "a + b should equal to zero");
 
     return 0;
 }
