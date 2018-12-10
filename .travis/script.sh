02a68e788fe0ca54c44d2432cb4226c53e2bfa55
MaiHD <maihd.dev@gmail.com>
2018-12-10 15:24:59 +0700

    Travis CI: shebang#2 for .travis/script.sh

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$BUILD_TARGET" == "android" ]; then  
+if [ "$BUILD_TARGET" == "android" ]; then
+    echo "Build Android: compiling without running unit tests."
     cd unit_tests/ndk-buid && ndk-build
-else  
+else
+    echo "Build target '$BUILD_TARGET': compile then run unit tests."
     cd unit_tests/gmake && make && make clean
 fi
