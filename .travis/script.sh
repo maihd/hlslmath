7a0042121fe86a30156d2803e42acda93a821a1d
MaiHD <maihd.dev@gmail.com>
2018-12-10 15:42:06 +0700

    Travis CI: absolate path to ndk-build

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,7 +2,7 @@
 
 if [ "$BUILD_TARGET" == "android" ]; then
     echo "Build Android: compiling without running unit tests."
-    cd unit_tests/ndkbuild && ndk-build
+    cd unit_tests/ndkbuild && $ANDROID_HOME/ndk-bundle/ndk-build
 else
     echo "Build target '$BUILD_TARGET': compile then run unit tests."
     cd unit_tests/gmake && make && make clean
