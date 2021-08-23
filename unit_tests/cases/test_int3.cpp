875e9e02d07a36b8f82db11050d680815f918331
maihd <maihd.dev@gmail.com>
2021-08-23 08:46:00 +0700

    test_uint2.cpp

---
+++ unit_tests/cases/test_int3.cpp
@@ -0,0 +1,27 @@
+#include <type_traits>
+
+#include "../test_framework.h"
+
+/// We don't care about: trivial, pod, standard layout.
+/// What we care is that int3 memcpyable, but standard layout is memcpyable.
+static_assert(!std::is_pod<int3>::value, "int3 is pod");
+static_assert(!std::is_trivial<int3>::value, "int3 is trivial");
+static_assert(std::is_standard_layout<int3>::value, "int3 is not trivial");
+
+/// This struct to prove that int3 is memcpyable
+union int3MustBeComposableInUnion
+{
+    int3 data;
+    int3 extraData;
+};
+
+DEFINE_UNIT_TEST("Working with int4")
+{
+    const int3 a = int3(0, 0, 0);
+    const int3 b = int3(0, 0, 0);
+
+    TEST(all(a == b), "a should equal to b");
+    TEST(!any(a != b), "a should equal to b");
+
+    TEST(all(a + b == int3(0, 0, 0)), "a + b should equal to zero");
+}
---
+++ unit_tests/cases/test_uint2.cpp
@@ -0,0 +1,27 @@
+#include <type_traits>
+
+#include "../test_framework.h"
+
+/// We don't care about: trivial, pod, standard layout.
+/// What we care is that uint2 memcpyable, but standard layout is memcpyable.
+static_assert(!std::is_pod<uint2>::value, "uint2 is pod");
+static_assert(!std::is_trivial<uint2>::value, "uint2 is trivial");
+static_assert(std::is_standard_layout<uint2>::value, "uint2 is not trivial");
+
+/// This struct to prove that int2 is memcpyable
+union unt2MustBeComposableInUnion
+{
+    uint2 data;
+    uint2 extraData;
+};
+
+DEFINE_UNIT_TEST("Working with uint2")
+{
+    const uint2 a = uint2(0, 0);
+    const uint2 b = uint2(0, 0);
+
+    TEST(all(a == b), "a should equal to b");
+    TEST(!any(a != b), "a should equal to b");
+
+    TEST(all(a + b == uint2(0, 0)), "a + b should equal to zero");
+}
